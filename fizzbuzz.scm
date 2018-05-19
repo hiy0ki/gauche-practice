a1f4168535a5efddd8b04247cce2e4d8eef05181
hiy0ki <hiyokix@gmail.com>
2018-05-19 13:44:36 +0900

    add sample

--- fizzbuzz.scm
+++ fizzbuzz.scm
@@ -10,4 +10,25 @@
   (map check (iota max 1)))
 
 
+(define (list?2 obj)
+  (or (null? obj)
+      (and (pair? obj) (list? (cdr obj)))))
+
+(fold + 0 '(1 2 3 4 5))
+
+(define (sum-of-numbers lis)
+  (fold + 0 lis))
+
+(define (product-of-numbers lis)
+  (fold * 1 lis))
+
+(define (max-number lis)
+  (define (pick-greater a b)
+    (if (> a b) a b))
+  (fold pick-greater -inf.0 lis))
+
+
+
+
+
         
