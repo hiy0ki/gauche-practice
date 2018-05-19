1900a680d83b3337bafb29f7cd2abf5ca6f94cfe
hiy0ki <hiyokix@gmail.com>
2018-05-19 22:40:09 +0900

    add list program

--- hello.scm
+++ hello.scm
@@ -15,3 +15,47 @@
 
 (define (pythagoras x y)
   (sqrt (+ (* x x) (* y y))))
+
+
+(define (fold2 proc init lis)
+  (if (null? lis)
+      init
+      (fold2 proc (car lis) init) (cdr lis))))
+
+
+(define (last-pair2 lis)
+  (if (pair? (cdr lis))
+      (last-pair2 (cdr lis))
+      lis))
+
+(define (copy-list lis)
+  (if (pair? lis)
+      (cons (car lis) (copy-list (cdr lis)))
+      lis))
+
+(define (append2 a b)
+  (if (pair? a)
+      (cons (car a) (append2 (cdr a) b))
+      b))
+
+(define (reverse1 lis)
+  (fold2 cons '() lis))
+
+(define (reverse2 lis)
+  (if (pair? lis)
+      (append2 (reverse2 (cdr lis)) (list (car lis)))
+      lis))
+
+
+(define (find2 pred lis)
+  (cond ((null? lis) #f)
+        ((pred (car lis)) (car lis))
+        (else (find2 pred (cdr lis)))))
+
+;; 末尾再帰バージョンのlength
+(define (length3 lis)
+  (define (length-rec lis n)
+    (if (null? lis)
+        n
+        (length-rec (cdr lis) (+ n 1))))
+  (length-rec lis 0))
