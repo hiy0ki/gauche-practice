8285424d831da5eeef8a8f4af15e3697cfd2db05
hiy0ki <hiyokix@gmail.com>
2018-07-16 23:00:09 +0900

    update date sample

--- date-and-time.scm
+++ date-and-time.scm
@@ -13,3 +13,19 @@
 
 (make-date 0 0 0 0 1 1 2018 (date-zone-offset (current-date)))
 
+(define (make-month m y)
+  (make-date 0 0 0 0 1 m y (date-zone-offset (current-date))))
+
+(define (first-day-of-month date)
+  (make-month (date-month date) (date-year date)))
+
+(define (next-month date)
+  (if (= (date-month date) 12)
+      (make-month 1 (+ (date-year date) 1))
+      (make-month (+ (date-month date) 1) (date-year date))))
+
+
+(define (prev-month date)
+  (if (= (date-month date) 1)
+      (make-month 12 (- (date-year date) 1))
+      (make-month (- (date-month date) 1) (date-year date))))
