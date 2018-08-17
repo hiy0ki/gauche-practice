41ffb489a5cbc480753c7f40c34609465ac3ee41
hiy0ki <hiyokix@gmail.com>
2018-08-17 13:36:18 +0900

    update black-jack

--- black-jack.scm
+++ black-jack.scm
@@ -1,5 +1,6 @@
-;; black jack
+;;; black jack game
 
+;; cards
 (define (create-card-from-simbol simbol numbers)
   (map (lambda (n)
          (cons simbol n))
@@ -32,6 +33,7 @@
 (define-method create-deck ((d <deck>))
   (set! (deck-of d) (card-shuffle (card-list))))
 
+;; todo: cardがない場合の処理 もう一回deckをつくる?
 (define-method draw-card ((d <deck>))
   (pop! (deck-of d)))
 
@@ -40,16 +42,16 @@
   ((name :init-value 'player :init-keyword :name :accessor name-of)
    (hands :init-value '() :init-keyword :hands :accessor hands-of)
    (point :init-value 0 :init-keyword :point :accessor point-of)
-   (stay? :init-value #f :init-keyword :stay? :accessor stay?-of)))
+   (draw? :init-value #t :init-keyword :draw? :accessor draw?-of)))
 
 (define-method add-hands ((p <player>) card)
   (set! (hands-of p) (append (list card) (ref p 'hands)))
   (add-point p))
 
-;; todo: j q k を点数丸める Aの扱いも追加する
 (define-method add-point ((p <player>))
   (set! (point-of p) (calc-point p)))
 
+;; todo: Aの扱いを追加する
 (define-method calc-point ((p <player>))
                     (+ (point-of p)
                        (let ((card-num (cdr (car (hands-of p)))))
@@ -64,16 +66,17 @@
 (define-method open-hands/mask ((p <player>))
   (append '("***") (cdr (hands-of p))))
 
+;; todo: show user point
 (define (show-field com usr)
   (print (name-of com) ":  " (open-hands/mask com))
   (print (name-of usr) ":  " (hands-of usr)))
 
 (define (show-result p1 p2)
-  (print "The winner is " (name-of (winner p1 p2)))
+  (print "The winner is " (name-of (winner p1 p2)) "!")
   (print (name-of p1) ":  point: " (point-of p1) " hands: " (hands-of p1))
   (print (name-of p2) ":  point: " (point-of p2) " hands: " (hands-of p2)))
 
-;; add if over 21
+;; todo: add if over 21
 (define (winner p1 p2)
   (if (>= (point-of p1) (point-of p2))
       p1
@@ -82,49 +85,38 @@
 (define (over-point-limit? point)
   (if (> point 21) #t #f))
 
-;; game loop
-;; com,user,deckはすでにある
-;; userのstay?
-;; if user selected false, draw card
-;; com stay?
-;; if com selected false, draw card
-;; next user turn
-
+;; game main
+;; todo userがstayしたらskipする
 (define (game com usr deck)
   (show-field com usr)
-  (display "stay?[y\/n]\n> ")
-  (let ((usr-stay? (read)))
+  (display "draw?[y\/n]\n> ")
+  (let ((usr-draw? (read)))
     (cond ((and
-            (if (stay?-of usr) #f #t)
-            (eq? usr-stay? 'n))
+            (draw?-of usr)
+            (eq? usr-draw? 'y))
            (print "usr draws card")
            (add-hands usr (draw-card deck)))
-          (else (set! (stay?-of usr) #t)))
-    (cond ((not (com-stay? com))
+          (else (set! (draw?-of usr) #f)))
+    (cond ((com-draw? com)
            (print "com draws card")
            (add-hands com (draw-card deck)))
-          (else (set! (stay?-of com) #t)))
-    (print "end")))
+          (else (set! (draw?-of com) #f)))
+    (print "turn end.")))
 
 ;; TODO もっとスッキリかけるはず
-(define (com-stay? com)
-  (if (or (> (point-of com) 17)
-          (stay?-of com))
-      #t
-      #f))
+(define (com-draw? com)
+  (and (<= (point-of com) 17)
+      (draw?-of com)))
 
 (define (next-turn? com usr)
-  (if (and (stay?-of com) (stay?-of usr))
-      #f
-      #t))
-
+  (or (draw?-of com) (draw?-of usr)))
 
 ;;; game
 ;; computer,userを作る
 ;; deckを作る
 ;; cardを配る
 ;; playerの順番をどうやって制御するか
-(define (game-test)
+(define (main args)
   (let ((com (make <player> :name 'computor))
         (usr (make <player> :name 'you))
         (deck (make <deck>)))
@@ -141,9 +133,4 @@
             (loop c u d)))
     (show-field com usr)
     (show-result com usr)))
-
-;; todo computerに依存させたほうが良さそう。
-(define (next-draw? p)
-  (or (<= (point-of p) 17)
-      (<= (point-of p) 21)))
 
